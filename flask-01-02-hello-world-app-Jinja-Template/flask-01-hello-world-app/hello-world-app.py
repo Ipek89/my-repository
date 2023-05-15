51bad238d4f7655a8f1781cf3c68ace97c67a09f
Ipek <ipekgokceig@gmail.com>
2023-05-14 22:48:41 -0400

    jinja templates

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -14,14 +14,37 @@
 
 #run the application in debug mode
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 def hello():
     return "<h1>Hello World</h1>"
 
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1=10, number2=20)
+
+
 @app.route('/')
 def hello():
-    return "h1>Hello World</h1>"
+    return "<h1>Hello World</h1>"
+@app.route('/second')
+def second():
+    return "h2>This is the second pafge</h2>"
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of the third page"
+
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'This is page id {id}'
 
 if __name__ == '__main__':
     app.run(debug=False)
+
+
+
+
+    
+
---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/flask-03-handling-routes/app.py
@@ -0,0 +1,23 @@
+# Import Flask modules
+from flask import Flask, render_template, redirect, url_for
+
+# Create an object named app
+app = Flask(__name__)
+
+# Create a function named home which returns a string 'This is home page for no path, <h1> Welcome Home</h1>' 
+# and assign route of no path ('/')
+@app.route('/')
+def home():
+    return 'This is home page for no path, <h1> Welcome Home</h1>'
+
+# Create a function named about which returns a formatted string '<h1>This is my about page </h1>' 
+# and assign to the static route of ('about')
+@app.route('/about')
+def about():
+    return '<h1>This is my about page </h1>'
+
+# Create a function named error which returns a formatted string '<h1>Either you encountered an error or you are not authorized.</h1>' 
+# and assign to the static route of ('error')
+@app.route('/error')
+def error():
+    return '<h1>Either you encountered an error or you are not authorized.</h1>'
